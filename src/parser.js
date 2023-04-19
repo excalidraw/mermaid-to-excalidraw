5c4a77c50b13a758268743ebac3cde7f06bc5b1e
chunza2542 <hello@thechun.dev>
2023-04-19 19:53:12 +0700

    Add nodeType

--- src/parser.js
+++ src/parser.js
@@ -3,9 +3,15 @@ export const parseNode = (node) => {
   const matrix = new DOMMatrixReadOnly(style.transform);
   const rect = node.getBoundingClientRect();
 
+  let nodeType;
+  if (node.querySelector("rect")) nodeType = "rect";
+  if (node.querySelector("circle")) nodeType = "circle";
+  if (node.querySelector("polygon")) nodeType = "polygon";
+
   return {
     id: node.id,
     type: "node",
+    nodeType,
     x: matrix.m41,
     y: matrix.m42,
     width: rect.width,
