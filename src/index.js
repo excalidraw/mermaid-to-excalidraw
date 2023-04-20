5f5a8785e6a09a204f65339861b1215c311b318c
chunza2542 <hello@thechun.dev>
2023-04-20 11:15:41 +0700

    Use mermaid from npm package instead

--- src/index.js
+++ src/index.js
@@ -1,37 +1,7 @@
 import { flowDiagrams } from "./flowDiagrams";
 import { parseRoot } from "./parser";
 import "./styles.css";
-const mermaid = window.mermaid;
-
-import m from "mermaid";
-
-(async () => {
-  const d = await m.mermaidAPI.getDiagramFromText(flowDiagrams[0]);
-  console.log(d);
-})();
-
-// initialize Mermaid
-mermaid.initialize({ startOnLoad: true });
-const container = document.getElementById("diagrams");
-
-// skips some diagrams #n
-const SKIPS = [39, 40];
-
-// render the diagram
-flowDiagrams.forEach((diagramDefinition, i) => {
-  if (SKIPS.includes(i + 1)) return;
-
-  const div = document.createElement("div");
-  div.id = `diagram-container-${i}`;
-  div.innerHTML = `<h1>Test #${
-    i + 1
-  }</h1><div id="diagram-${i}"></div><pre id="parsed-${i}"></pre>`;
-  const dg = div.querySelector(`#diagram-${i}`);
-
-  const svg = mermaid.mermaidAPI.render(`diagram-${i}`, diagramDefinition);
-  dg.innerHTML = svg;
-  container.append(div);
-});
+import mermaid from "mermaid";
 
 // TODO: how to connect label with edge since label have no id attached.
 //    sol1: get graph data from parser (didn't find public function for this yet)
@@ -41,16 +11,67 @@ flowDiagrams.forEach((diagramDefinition, i) => {
 // TODO: how to render arrow curve in Excalidraw
 //    sol: use "curve": "linear" options, find a way to detect breaking point -> replicate on Excalidraw
 
-// implement the parser
-flowDiagrams.forEach((diagramDefinition, i) => {
+// initialize Mermaid
+mermaid.initialize({ startOnLoad: false });
+const container = document.getElementById("diagrams");
+
+// skips some diagrams #n
+const SKIPS = [39, 40];
+
+// render the diagram
+flowDiagrams.forEach(async (diagramDefinition, i) => {
   if (SKIPS.includes(i + 1)) return;
 
-  const div = document.querySelector(`#diagram-container-${i}`);
-  const p = div.querySelector(`#parsed-${i}`);
+  const div = document.createElement("div");
+  div.id = `diagram-container-${i}`;
+  div.innerHTML = `<h1>Test #${
+    i + 1
+  }</h1><div id="diagram-${i}"></div><pre id="parsed-${i}"></pre>`;
+
+  const dg = div.querySelector(`#diagram-${i}`);
+  const { svg } = await mermaid.render(`diagram-${i}`, diagramDefinition);
+
+  dg.innerHTML = svg;
+  container.append(div);
+});
+
+// implement the parser
+// import m from "mermaid";
+// m.initialize({ startOnLoad: false });
+// let diagram;
+flowDiagrams.forEach(async (diagramDefinition, i) => {
+  if (SKIPS.includes(i + 1)) return;
+  // if (!diagram) {
+  //   diagram = await m.mermaidAPI.getDiagramFromText("flowchart");
+  //   console.log("...");
+  // }
+
+  // const div = document.querySelector(`#diagram-container-${i}`);
+  // const p = div.querySelector(`#parsed-${i}`);
+
+  // m.mermaidAPI.render(`diagram-${i}`, diagramDefinition);
+
+  // const {
+  //   getAccDescription,
+  //   getAccTitle,
+  //   getClasses,
+  //   getDepthFirstPos,
+  //   getDiagramTitle,
+  //   getDirection,
+  //   getEdges,
+  //   getSubGraphs,
+  //   getTooltip,
+  //   getVertices,
+  // } = diagram.parser.yy;
+
+  // console.log(diagram, m);
+  // console.log(i + 1, getAccDescription(), getAccTitle(), getVertices());
 
   // parse relevant info from SVG
-  const root = parseRoot(div);
+  // const root = parseRoot(div);
 
   // parsed data to Excalidraw element
-  p.innerHTML = JSON.stringify(root, null, 2);
+  // p.innerHTML = JSON.stringify(root, null, 2);
+
+  // diagram.parser.yy.clear();
 });
