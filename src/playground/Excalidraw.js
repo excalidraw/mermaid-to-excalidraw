e56aeadb9ec43a88153cbb4f69bd023c1f62c9b9
chunza2542 <hello@thechun.dev>
2023-05-01 14:23:24 +0700

    Remove unused code

--- src/playground/Excalidraw.js
+++ src/playground/Excalidraw.js
@@ -1,60 +1,5 @@
 export const Excalidraw = (props) => {
   const excalidrawRef = React.useRef(null);
-  const excalidrawWrapperRef = React.useRef(null);
-  const [dimensions, setDimensions] = React.useState({
-    width: undefined,
-    height: undefined,
-  });
-
-  React.useEffect(() => {
-    setDimensions({
-      width: excalidrawWrapperRef.current.getBoundingClientRect().width,
-      height: excalidrawWrapperRef.current.getBoundingClientRect().height,
-    });
-    const onResize = () => {
-      setDimensions({
-        width: excalidrawWrapperRef.current.getBoundingClientRect().width,
-        height: excalidrawWrapperRef.current.getBoundingClientRect().height,
-      });
-    };
-
-    window.addEventListener("resize", onResize);
-
-    return () => window.removeEventListener("resize", onResize);
-  }, [excalidrawWrapperRef]);
-
-  const updateScene = () => {
-    const sceneData = {
-      elements: [
-        {
-          type: "rectangle",
-          version: 141,
-          versionNonce: 361174001,
-          isDeleted: false,
-          id: "oDVXy8D6rom3H1-LLH2-f",
-          fillStyle: "hachure",
-          strokeWidth: 1,
-          strokeStyle: "solid",
-          roughness: 1,
-          opacity: 100,
-          angle: 0,
-          x: 100.50390625,
-          y: 93.67578125,
-          strokeColor: "#c92a2a",
-          backgroundColor: "transparent",
-          width: 186.47265625,
-          height: 141.9765625,
-          seed: 1968410350,
-          groupIds: [],
-        },
-      ],
-      appState: {
-        viewBackgroundColor: "#fafafa",
-      },
-    };
-    console.log(excalidrawRef, "hello");
-    excalidrawRef.current.updateScene(sceneData);
-  };
 
   return React.createElement(
     React.Fragment,
@@ -63,7 +8,6 @@ export const Excalidraw = (props) => {
       "div",
       {
         className: "excalidraw-wrapper",
-        ref: excalidrawWrapperRef,
       },
       React.createElement(ExcalidrawLib.Excalidraw, {
         initialData: {
