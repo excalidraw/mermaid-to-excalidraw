82ebf80c88ad819c5ad38ff2212e496ab0d2d8bd
chunza2542 <hello@thechun.dev>
2023-05-05 15:40:14 +0700

    Remove test

--- src/tests/mermaidToExcalidraw.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, it } from "vitest";
-import mermaidToExcalidraw from "../mermaidToExcalidraw.js";
-
-it("mermaidToExcalidraw", async () => {
-  const elements = await mermaidToExcalidraw("flowchart TD\nStart --> Stop");
-  expect(elements.length).toBe(3);
-});
