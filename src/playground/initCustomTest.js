bb7223571ae814e2f569be2a4d18991787c15075
chunza2542 <hello@thechun.dev>
2023-05-26 15:19:19 +0700

    Add custom test description

--- src/playground/initCustomTest.js
+++ src/playground/initCustomTest.js
@@ -7,6 +7,11 @@ const customTestEl = document.createElement("div");
 const containerEl = document.getElementById("diagrams");
 customTestEl.innerHTML = `
   <h1>Custom Test</h1>
+  <ul>
+    <li>Support only flowchart diagram (the input must started with "flowchart")</li>
+    <li>See supported and unsupported features at <a target="_blank" href="https://github.com/excalidraw/mermaid-to-excalidraw/pull/1#issue-1686226562">PR's description</a></li>
+  </ul>
+  <br>
   <textarea id="mermaid-input" rows="10" cols="50"></textarea><br>
   <label for="font-size-input">Custom Font Size: </label> <input type="number" id="font-size-input" value="${DEFAULT_FONT_SIZE}"><br>
   <button id="render-excalidraw-btn">Render to Excalidraw</button>
