ebeacd5739ccd53a8e388a0a3fcde284af030908
chunza2542 <hello@thechun.dev>
2023-06-09 13:08:21 +0700

    Handle err for custom test

--- playground/initCustomTest.ts
+++ playground/initCustomTest.ts
@@ -21,6 +21,7 @@ customTestEl.innerHTML = `
     <summary>Parsed data from parseMermaid</summary>
     <pre id="custom-parsed-data"></pre>
   </details>
+  <div id="error" style="display: none; color: red; margin-top: 20px;"></div>
 `;
 document.body.appendChild(customTestEl);
 
@@ -30,36 +31,43 @@ if (!btn) {
   throw new Error("Button element not found");
 }
 
+const errorEl: HTMLElement | null = customTestEl.querySelector("#error");
+if (!errorEl) {
+  throw new Error("Error element not found");
+}
+
 btn.addEventListener("click", async () => {
-  // Get inputs
-  const mermaidInput = document.getElementById(
-    "mermaid-input"
-  ) as HTMLInputElement;
-  if (!mermaidInput) {
-    throw new Error("Mermaid input element not found");
-  }
-  const fontSizeInput = document.getElementById(
-    "font-size-input"
-  ) as HTMLInputElement;
-  if (!fontSizeInput) {
-    throw new Error("Font size input element not found");
-  }
-  const diagramDefinition = mermaidInput.value;
-  const customFontSize = Number(fontSizeInput.value);
+  errorEl.style.display = "none";
 
-  // Render Mermaid diagram
-  const diagramEl = document.getElementById("custom-diagram");
-  if (!diagramEl) {
-    throw new Error("Diagram element not found");
-  }
-  const { svg } = await mermaid.render(
-    `custom-digaram`,
-    `%%{init: {"themeVariables": {"fontSize": "${customFontSize}px"}} }%%\n${diagramDefinition}`
-  );
-  diagramEl.innerHTML = svg;
-
-  // Parse Mermaid diagram and render to Excalidraw
   try {
+    // Get inputs
+    const mermaidInput = document.getElementById(
+      "mermaid-input"
+    ) as HTMLInputElement;
+    if (!mermaidInput) {
+      throw new Error("Mermaid input element not found");
+    }
+    const fontSizeInput = document.getElementById(
+      "font-size-input"
+    ) as HTMLInputElement;
+    if (!fontSizeInput) {
+      throw new Error("Font size input element not found");
+    }
+    const diagramDefinition = mermaidInput.value;
+    const customFontSize = Number(fontSizeInput.value);
+
+    // Render Mermaid diagram
+    const diagramEl = document.getElementById("custom-diagram");
+    if (!diagramEl) {
+      throw new Error("Diagram element not found");
+    }
+    const { svg } = await mermaid.render(
+      `custom-digaram`,
+      `%%{init: {"themeVariables": {"fontSize": "${customFontSize}px"}} }%%\n${diagramDefinition}`
+    );
+    diagramEl.innerHTML = svg;
+
+    // Parse Mermaid diagram and render to Excalidraw
     const parsedData = await parseMermaid(mermaid, diagramDefinition, {
       fontSize: customFontSize,
     });
@@ -74,7 +82,9 @@ btn.addEventListener("click", async () => {
     parsedDataEl.innerText = JSON.stringify(parsedData, null, 2);
 
     renderExcalidraw(JSON.stringify(parsedData), customFontSize);
-  } catch (e) {
+  } catch (e: any) {
+    errorEl.style.display = "block";
+    errorEl.innerHTML = String(e);
     console.error("Custom Test Error:", e);
   }
 });
