5ef58aa7c7c43c1560bfd21ef1d03ca9dfbf6844
chunza2542 <hello@thechun.dev>
2023-06-30 18:32:33 +0700

    Apply fitToContent

--- playground/ExcalidrawWrapper.ts
+++ playground/ExcalidrawWrapper.ts
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useEffect, useMemo } from "react";
 import { Excalidraw } from "@excalidraw/excalidraw";
-import { BinaryFiles } from "@excalidraw/excalidraw/types/types";
+import {
+  BinaryFiles,
+  ExcalidrawImperativeAPI,
+} from "@excalidraw/excalidraw/types/types";
 import { ExcalidrawElement } from "../src/types";
 
 interface ExcalidrawWrapperProps {
   elements: ExcalidrawElement[];
   files?: BinaryFiles;
 }
+const resolvablePromise = () => {
+  let resolve;
+  let reject;
+  const promise: any = new Promise((_resolve, _reject) => {
+    resolve = _resolve;
+    reject = _reject;
+  });
+  promise.resolve = resolve;
+  promise.reject = reject;
+  return promise;
+};
 const ExcalidrawWrapper = (props: ExcalidrawWrapperProps) => {
-  const excalidrawRef = React.useRef(null);
+  const excalidrawRef = useMemo(
+    () => ({
+      current: {
+        readyPromise: resolvablePromise(),
+      },
+    }),
+    []
+  );
+
+  useEffect(() => {
+    excalidrawRef.current.readyPromise.then(
+      (excalidrawAPI: ExcalidrawImperativeAPI) => {
+        setTimeout(() => {
+          excalidrawAPI.updateScene({
+            elements: props.elements,
+          });
+          excalidrawAPI.scrollToContent(excalidrawAPI.getSceneElements(), {
+            fitToContent: true,
+          });
+        }, 0);
+      }
+    );
+  }, [excalidrawRef.current]);
 
   return React.createElement(
     React.Fragment,
@@ -20,13 +56,11 @@ const ExcalidrawWrapper = (props: ExcalidrawWrapperProps) => {
       },
       React.createElement(Excalidraw, {
         initialData: {
-          elements: props.elements,
           files: props.files,
           appState: {
             viewBackgroundColor: "#fafafa",
             currentItemFontFamily: 1,
           },
-          scrollToContent: true,
         },
         ref: excalidrawRef,
       })
