a41e6aa07d4976ea65f9ac3018f31dae3cd4f6cb
chunza2542 <hello@thechun.dev>
2023-06-30 19:02:00 +0700

    Add unsupported diagram section

--- playground/testcases/flowchart.ts
+++ playground/testcases/flowchart.ts
@@ -212,19 +212,4 @@ C -->|Two| E[Result two]
   H -->|No| J[Action 6]
   I --> K[End]
   J --> K`,
-
-  // Unsupported diagram testcases
-  `erDiagram
-    CUSTOMER ||--o{ ORDER : places
-    ORDER ||--|{ LINE-ITEM : contains
-    CUSTOMER }|..|{ DELIVERY-ADDRESS : uses`,
-  `gantt
-    title A Gantt Diagram
-    dateFormat  YYYY-MM-DD
-    section Section
-    A task           :a1, 2014-01-01, 30d
-    Another task     :after a1  , 20d
-    section Another
-    Task in sec      :2014-01-12  , 12d
-    another task      : 24d`,
 ];
