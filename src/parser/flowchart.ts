58051dcfb5f6cc8c510feeebb6d3a942c1859313
Aakansha Doshi <aakansha1216@gmail.com>
2023-09-13 15:39:20 +0530

    refactor: move parser logic to parser folder (#22)

---
+++ src/parser/flowchart.ts
@@ -0,0 +1,285 @@
+import { entityCodesToText, getTransformAttr } from "../utils";
+import {
+  CONTAINER_STYLE_PROPERTY,
+  Edge,
+  Graph,
+  LABEL_STYLE_PROPERTY,
+  Position,
+  SubGraph,
+  Vertex,
+} from "../interfaces";
+
+import { Diagram } from "mermaid/dist/Diagram";
+
+const parseSubGraph = (data: any, containerEl: Element): SubGraph => {
+  // Extract only node id for better reference
+  // e.g. full element id = "flowchart-c1-205" will map to "c1"
+  const nodeIds = data.nodes.map((n: string) => {
+    if (n.startsWith("flowchart-")) {
+      return n.split("-")[1];
+    }
+    return n;
+  });
+
+  // Get position
+  const el: SVGSVGElement | null = containerEl.querySelector(
+    `[id='${data.id}']`
+  );
+  if (!el) {
+    throw new Error("SubGraph element not found");
+  }
+  const position = computeElementPosition(el, containerEl);
+
+  // Get dimension
+  const boundingBox = el.getBBox();
+  const dimension = {
+    width: boundingBox.width,
+    height: boundingBox.height,
+  };
+
+  // Remove irrelevant properties
+  data.classes = undefined;
+  data.dir = undefined;
+
+  return {
+    ...data,
+    nodeIds,
+    ...position,
+    ...dimension,
+    text: entityCodesToText(data.title),
+  };
+};
+
+const parseVertex = (data: any, containerEl: Element): Vertex | undefined => {
+  // Find Vertex element
+  const el: SVGSVGElement | null = containerEl.querySelector(
+    `[id*="flowchart-${data.id}-"]`
+  );
+  if (!el) {
+    return undefined;
+  }
+
+  // Check if Vertex attached with link
+  let link;
+  if (el.parentElement?.tagName.toLowerCase() === "a") {
+    link = el.parentElement.getAttribute("xlink:href");
+  }
+
+  // Get position
+  const position = computeElementPosition(
+    link ? el.parentElement : el,
+    containerEl
+  );
+  // Get dimension
+  const boundingBox = el.getBBox();
+  const dimension = {
+    width: boundingBox.width,
+    height: boundingBox.height,
+  };
+
+  // Extract style
+  const labelContainerStyleText = el
+    .querySelector(".label-container")
+    ?.getAttribute("style");
+  const labelStyleText = el.querySelector(".label")?.getAttribute("style");
+
+  const containerStyle: Vertex["containerStyle"] = {};
+  labelContainerStyleText?.split(";").forEach((property) => {
+    if (!property) {
+      return;
+    }
+
+    const key = property.split(":")[0].trim() as CONTAINER_STYLE_PROPERTY;
+    const value = property.split(":")[1].trim();
+    containerStyle[key] = value;
+  });
+  const labelStyle: Vertex["labelStyle"] = {};
+  labelStyleText?.split(";").forEach((property) => {
+    if (!property) {
+      return;
+    }
+
+    const key = property.split(":")[0].trim() as LABEL_STYLE_PROPERTY;
+    const value = property.split(":")[1].trim();
+    labelStyle[key] = value;
+  });
+
+  return {
+    id: data.id,
+    labelType: data.labelType,
+    text: entityCodesToText(data.text),
+    type: data.type,
+    link: link || undefined,
+    ...position,
+    ...dimension,
+    containerStyle,
+    labelStyle,
+  };
+};
+
+const parseEdge = (data: any, containerEl: Element): Edge => {
+  // Find edge element
+  const el: SVGPathElement | null = containerEl.querySelector(
+    `[id*="L-${data.start}-${data.end}"]`
+  );
+  if (!el) {
+    throw new Error("Edge element not found");
+  }
+
+  // Compute edge position data
+  const position = computeElementPosition(el, containerEl);
+  const edgePositionData = computeEdgePositions(el, position);
+
+  // Remove irrelevant properties
+  data.length = undefined;
+
+  return {
+    ...data,
+    ...edgePositionData,
+    text: entityCodesToText(data.text),
+  };
+};
+
+// Compute element position
+const computeElementPosition = (
+  el: Element | null,
+  containerEl: Element
+): Position => {
+  if (!el) {
+    throw new Error("Element not found");
+  }
+
+  let root = el.parentElement?.parentElement;
+
+  const childElement = el.childNodes[0] as SVGSVGElement;
+  let childPosition = { x: 0, y: 0 };
+  if (childElement) {
+    const { transformX, transformY } = getTransformAttr(childElement);
+
+    const boundingBox = childElement.getBBox();
+    childPosition = {
+      x:
+        Number(childElement.getAttribute("x")) ||
+        transformX + boundingBox.x ||
+        0,
+      y:
+        Number(childElement.getAttribute("y")) ||
+        transformY + boundingBox.y ||
+        0,
+    };
+  }
+
+  const { transformX, transformY } = getTransformAttr(el);
+  const position = {
+    x: transformX + childPosition.x,
+    y: transformY + childPosition.y,
+  };
+  while (root && root.id !== containerEl.id) {
+    if (root.classList.value === "root" && root.hasAttribute("transform")) {
+      const { transformX, transformY } = getTransformAttr(root);
+      position.x += transformX;
+      position.y += transformY;
+    }
+
+    root = root.parentElement;
+  }
+
+  return position;
+};
+
+// Extract edge position start, end, and points (reflectionPoints)
+interface EdgePositionData {
+  startX: number;
+  startY: number;
+  endX: number;
+  endY: number;
+  reflectionPoints: Position[];
+}
+const computeEdgePositions = (
+  pathElement: SVGPathElement,
+  offset: Position = { x: 0, y: 0 }
+): EdgePositionData => {
+  if (pathElement.tagName.toLowerCase() !== "path") {
+    throw new Error(
+      `Invalid input: Expected an HTMLElement of tag "path", got ${pathElement.tagName}`
+    );
+  }
+
+  const dAttr = pathElement.getAttribute("d");
+  if (!dAttr) {
+    throw new Error('Path element does not contain a "d" attribute');
+  }
+
+  // Split the d attribute based on M (Move To) and L (Line To) commands
+  const commands = dAttr.split(/(?=[LM])/);
+  const startPosition = commands[0]
+    .substring(1)
+    .split(",")
+    .map((coord) => parseFloat(coord));
+  const endPosition = commands[commands.length - 1]
+    .substring(1)
+    .split(",")
+    .map((coord) => parseFloat(coord));
+  const reflectionPoints = commands
+    .map((command) => {
+      const coords = command
+        .substring(1)
+        .split(",")
+        .map((coord) => parseFloat(coord));
+      return { x: coords[0], y: coords[1] };
+    })
+    .filter((point, index, array) => {
+      if (index === array.length - 1) {
+        return true;
+      }
+      const prevPoint = array[index - 1];
+      return (
+        index === 0 || (point.x !== prevPoint.x && point.y !== prevPoint.y)
+      );
+    })
+    .map((p) => {
+      return {
+        x: p.x + offset.x,
+        y: p.y + offset.y,
+      };
+    });
+
+  return {
+    startX: startPosition[0] + offset.x,
+    startY: startPosition[1] + offset.y,
+    endX: endPosition[0] + offset.x,
+    endY: endPosition[1] + offset.y,
+    reflectionPoints,
+  };
+};
+
+export const parseMermaidFlowChartDiagram = (
+  diagram: Diagram,
+  containerEl: Element
+): Graph => {
+  // This does some cleanup and initialization making sure
+  // diagram is parsed correctly. Useful when multiple diagrams are
+  // parsed together one after another, eg in playground
+  // https://github.com/mermaid-js/mermaid/blob/e561cbd3be2a93b8bedfa4839484966faad92ccf/packages/mermaid/src/Diagram.ts#L43
+  diagram.parse();
+
+  // Get mermaid parsed data from parser shared variable `yy`
+  const mermaidParser = diagram.parser.yy;
+  const vertices = mermaidParser.getVertices();
+  Object.keys(vertices).forEach((id) => {
+    vertices[id] = parseVertex(vertices[id], containerEl);
+  });
+  const edges = mermaidParser
+    .getEdges()
+    .map((data: any) => parseEdge(data, containerEl));
+  const subGraphs = mermaidParser
+    .getSubGraphs()
+    .map((data: any) => parseSubGraph(data, containerEl));
+
+  return {
+    type: "flowchart",
+    subGraphs,
+    vertices,
+    edges,
+  };
+};
