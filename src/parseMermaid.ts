60280138867dc480eb2f8d196f6d550101787053
Aakansha Doshi <aakansha1216@gmail.com>
2023-09-13 09:25:03 +0530

    refactor: decouple appending svg to dom and cleanup (#20)
    * Move the logic of appending svg to DOM outside switch as its common to both (supported and unsupported diagrams)
    * Reorder the functions - move function definitions before being called for better readability

--- src/parseMermaid.ts
+++ src/parseMermaid.ts
@@ -5,93 +5,6 @@ import { MermaidOptions } from ".";
 import { parseMermaidFlowChartDiagram } from "./converter/types/flowchart";
 import { isSupportedDiagram } from "./utils";
 
-export const parseMermaid = async (
-  definition: string,
-  options: MermaidOptions = {}
-): Promise<Graph | GraphImage> => {
-  mermaid.initialize({ startOnLoad: false });
-
-  const fullDefinition = processMermaidTextWithOptions(definition, {
-    curve: isSupportedDiagram(definition) ? "linear" : "basis",
-    fontSize: options.fontSize,
-  });
-  // Parse the diagram
-  const diagram = await mermaid.mermaidAPI.getDiagramFromText(fullDefinition);
-
-  // Render the SVG diagram
-  const { svg } = await mermaid.render("mermaid-to-excalidraw", fullDefinition);
-
-  let data;
-  switch (diagram.type) {
-    case "flowchart-v2": {
-      const diagramEl = document.createElement("div");
-      diagramEl.setAttribute(
-        "style",
-        `opacity: 0; position: relative; z-index: -1;`
-      );
-      diagramEl.innerHTML = svg;
-      diagramEl.id = "mermaid-diagram";
-      document.body.appendChild(diagramEl);
-
-      data = parseMermaidFlowChartDiagram(diagram, diagramEl);
-      diagramEl.remove();
-      break;
-    }
-    // fallback to image if diagram type not-supported
-    default: {
-      data = convertSvgToGraphImage(svg);
-    }
-  }
-  return data;
-};
-
-/* Helper Functions */
-
-// Fallback to Svg
-const convertSvgToGraphImage = (svg: string) => {
-  // Extract SVG width and height
-  // TODO: make width and height change dynamically based on user's screen dimension
-  const svgContainer = document.createElement("div");
-  svgContainer.innerHTML = svg;
-  svgContainer.setAttribute(
-    "style",
-    `opacity: 0; position: relative; z-index: -1;`
-  );
-  document.body.appendChild(svgContainer);
-  const svgEl = svgContainer.querySelector("svg");
-  if (!svgEl) {
-    throw new Error("SVG element not found");
-  }
-  const rect = svgEl.getBoundingClientRect();
-  const width = rect.width;
-  const height = rect.height;
-
-  // Set width and height explictly since in firefox it gets set to 0
-  // if the width and height are not expilcitly set
-  // eg in some cases like er Diagram, gnatt, width and height is set as 100%
-  // which sets the dimensions as 0 in firefox and thus the diagram isn't rendered
-  svgEl.setAttribute("width", `${width}`);
-  svgEl.setAttribute("height", `${height}`);
-
-  svgContainer.remove();
-
-  // Convert SVG to image
-  const mimeType = "image/svg+xml";
-  const decoded = unescape(encodeURIComponent(svgEl.outerHTML));
-  const base64 = btoa(decoded);
-  const dataURL = `data:image/svg+xml;base64,${base64}`;
-
-  const graphImage: GraphImage = {
-    type: "graphImage",
-    mimeType,
-    dataURL,
-    width,
-    height,
-  };
-
-  return graphImage;
-};
-
 interface MermaidDefinitionOptions {
   curve?: "linear" | "basis";
   fontSize?: number;
@@ -116,4 +29,82 @@ const processMermaidTextWithOptions = (
   )}}%%\n${definition}`;
 
   return fullDefinition;
+};
+
+// Fallback to Svg
+const convertSvgToGraphImage = (svgContainer: HTMLDivElement) => {
+  // Extract SVG width and height
+  // TODO: make width and height change dynamically based on user's screen dimension
+  const svgEl = svgContainer.querySelector("svg");
+  if (!svgEl) {
+    throw new Error("SVG element not found");
+  }
+  const rect = svgEl.getBoundingClientRect();
+  const width = rect.width;
+  const height = rect.height;
+
+  // Set width and height explictly since in firefox it gets set to 0
+  // if the width and height are not expilcitly set
+  // eg in some cases like er Diagram, gnatt, width and height is set as 100%
+  // which sets the dimensions as 0 in firefox and thus the diagram isn't rendered
+  svgEl.setAttribute("width", `${width}`);
+  svgEl.setAttribute("height", `${height}`);
+
+  // Convert SVG to image
+  const mimeType = "image/svg+xml";
+  const decoded = unescape(encodeURIComponent(svgEl.outerHTML));
+  const base64 = btoa(decoded);
+  const dataURL = `data:image/svg+xml;base64,${base64}`;
+
+  const graphImage: GraphImage = {
+    type: "graphImage",
+    mimeType,
+    dataURL,
+    width,
+    height,
+  };
+
+  return graphImage;
+};
+
+export const parseMermaid = async (
+  definition: string,
+  options: MermaidOptions = {}
+): Promise<Graph | GraphImage> => {
+  mermaid.initialize({ startOnLoad: false });
+
+  const fullDefinition = processMermaidTextWithOptions(definition, {
+    curve: isSupportedDiagram(definition) ? "linear" : "basis",
+    fontSize: options.fontSize,
+  });
+  // Parse the diagram
+  const diagram = await mermaid.mermaidAPI.getDiagramFromText(fullDefinition);
+
+  // Render the SVG diagram
+  const { svg } = await mermaid.render("mermaid-to-excalidraw", fullDefinition);
+
+  // Append Svg to DOM
+  const svgContainer = document.createElement("div");
+  svgContainer.setAttribute(
+    "style",
+    `opacity: 0; position: relative; z-index: -1;`
+  );
+  svgContainer.innerHTML = svg;
+  svgContainer.id = "mermaid-diagram";
+  document.body.appendChild(svgContainer);
+
+  let data;
+  switch (diagram.type) {
+    case "flowchart-v2": {
+      data = parseMermaidFlowChartDiagram(diagram, svgContainer);
+      break;
+    }
+    // fallback to image if diagram type not-supported
+    default: {
+      data = convertSvgToGraphImage(svgContainer);
+    }
+  }
+  svgContainer.remove();
+
+  return data;
 };
