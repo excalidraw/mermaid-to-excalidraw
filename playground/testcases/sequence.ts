7aa12c0d22f3b2d47b7435caa031692465b4f822
Aakansha Doshi <aakansha1216@gmail.com>
2023-10-12 13:35:25 +0530

    add playground for sequence

---
+++ playground/testcases/sequence.ts
@@ -0,0 +1,32 @@
+const SEQUENCE_DIAGRAM_TESTCASES = [
+  {
+    name: "Solid and dotted line without arrow",
+    defination: `sequenceDiagram
+  Alice->John: Hello John, how are you?
+  John-->Alice: Great!
+  Alice->John: See you later!`,
+  },
+  {
+    name: "Solid and dotted line with arrow head",
+    defination: `sequenceDiagram
+  Alice->>John: Hello John, how are you?
+  John-->>Alice: Great!
+  Alice->>John: See you later!`,
+  },
+  {
+    name: "Solid and Dotted line with cross at end",
+    defination: `sequenceDiagram
+  Alice-xJohn: Hello John, how are you?
+  John--xAlice: Great!
+  Alice-xJohn: See you later!`,
+  },
+  {
+    name: "Solid and dotted line with open arrow at end",
+    defination: `sequenceDiagram
+  Alice-)John: Hello John, how are you?
+  John--)Alice: Great!
+  Alice-)John: See you later!`,
+  },
+];
+
+export { SEQUENCE_DIAGRAM_TESTCASES };
