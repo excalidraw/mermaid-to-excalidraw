48245fd8d78ce8a82f89868655079180bb35a6a4
Igor Wessel <igor_wessel@hotmail.com>
2024-04-11 14:35:32 -0300

    build: add preview script to see production build

--- playground/vite.config.js
+++ playground/vite.config.js
@@ -3,5 +3,25 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     outDir: "../public",
+    emptyOutDir: true,
+  },
+  server: {
+    warmup: {
+      /* 
+        A small performance improvement so that this file is already transformed, cached when we receive the request :)
+        See more: https://vitejs.dev/guide/performance.html#warm-up-frequently-used-files
+      */
+      clientFiles: [
+        "../src/graphToExcalidraw.ts",
+        "./initExcalidraw.ts",
+        "../src/parser/class.ts",
+        "../src/parser/flowchart.ts",
+        "../src/parser/sequence.ts",
+        "./testcases/class.ts",
+        "./testcases/flowchart.ts",
+        "./testcases/sequence.ts",
+        "./testcases/unsupported.ts",
+      ],
+    },
   },
 });
