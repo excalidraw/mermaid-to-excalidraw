c87ee0443f5a24b8356bd4ddb5a0911a651e60ca
Aakansha Doshi <aakansha1216@gmail.com>
2024-04-25 11:33:13 +0530

    cleanup Testcases.tsx

--- playground/Testcases.tsx
+++ playground/Testcases.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, Fragment } from "react";
 
 import { FLOWCHART_DIAGRAM_TESTCASES } from "./testcases/flowchart";
 import { SEQUENCE_DIAGRAM_TESTCASES } from "./testcases/sequence.ts";
@@ -9,13 +9,13 @@ import { Mermaid } from "./Mermaid";
 
 interface TestCaseProps {
   name: string;
-  baseId: string;
   testcases: { name: string; definition: string }[];
   onChange: (definition: string) => void;
 }
 
-const Testcase = ({ name, baseId, testcases, onChange }: TestCaseProps) => {
+const Testcase = ({ name, testcases, onChange }: TestCaseProps) => {
   const activeTestcase = useRef<number>();
+  const baseId = name.toLowerCase();
 
   useEffect(() => {
     const testcase = activeTestcase.current;
@@ -29,31 +29,20 @@ const Testcase = ({ name, baseId, testcases, onChange }: TestCaseProps) => {
 
   return (
     <>
-      <h2>{`${name} Diagrams`}</h2>
+      <h2>
+        {name} {"Diagrams"}
+      </h2>
       <details>
-        <summary>{`${name} Examples`}</summary>
-        <div id={`${baseId}-container`}>
+        <summary>
+          {name} {"Examples"}
+        </summary>
+        <div id={`${baseId}-container`} className="testcase-container">
           {testcases.map(({ name, definition }, index) => {
             const id = `${baseId}-${index}`;
-
             return (
-              <div key={id}>
+              <Fragment key={id}>
                 <h2 style={{ marginTop: "50px", color: "#f06595" }}>{name}</h2>
-
-                <pre
-                  style={{
-                    fontSize: "16px",
-                    fontWeight: "600",
-                    fontStyle: "italic",
-                    background: "#eeeef1",
-                    whiteSpace: "pre-wrap",
-                    width: "45vw",
-                    padding: "5px",
-                  }}
-                >
-                  {definition}
-                </pre>
-
+                <pre>{definition}</pre>
                 <button
                   onClick={() => {
                     onChange(definition);
@@ -63,9 +52,8 @@ const Testcase = ({ name, baseId, testcases, onChange }: TestCaseProps) => {
                 >
                   {"Render to Excalidraw"}
                 </button>
-
                 <Mermaid definition={definition} id={id} />
-              </div>
+              </Fragment>
             );
           })}
         </div>
@@ -79,35 +67,23 @@ interface TestcasesProps {
 }
 
 const Testcases = ({ onChange }: TestcasesProps) => {
+  const testCases = [
+    { name: "Flowchart", testcases: FLOWCHART_DIAGRAM_TESTCASES },
+    { name: "Sequence", testcases: SEQUENCE_DIAGRAM_TESTCASES },
+    { name: "Class", testcases: CLASS_DIAGRAM_TESTCASES },
+    { name: "Unsupported", testcases: UNSUPPORTED_DIAGRAM_TESTCASES },
+  ];
+
   return (
     <>
-      <Testcase
-        name="Flowchart"
-        baseId="flowchart"
-        onChange={onChange}
-        testcases={FLOWCHART_DIAGRAM_TESTCASES}
-      />
-
-      <Testcase
-        name="Sequence"
-        baseId="sequence"
-        onChange={onChange}
-        testcases={SEQUENCE_DIAGRAM_TESTCASES}
-      />
-
-      <Testcase
-        name="Class"
-        baseId="class"
-        onChange={onChange}
-        testcases={CLASS_DIAGRAM_TESTCASES}
-      />
-
-      <Testcase
-        name="Unsupported"
-        baseId="unsupported"
-        onChange={onChange}
-        testcases={UNSUPPORTED_DIAGRAM_TESTCASES}
-      />
+      {testCases.map(({ name, testcases }, index) => (
+        <Testcase
+          key={index}
+          name={name}
+          onChange={onChange}
+          testcases={testcases}
+        />
+      ))}
     </>
   );
 };
