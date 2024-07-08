406202dc8b447e6abfb549e638e4d2d4effb65a0
Aakansha Doshi <aakansha1216@gmail.com>
2024-07-08 16:57:24 +0530

    add link

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,7 @@ export const SVG_TO_SHAPE_MAPPER: { [key: string]: "rectangle" | "ellipse" } = {
   circle: "ellipse",
 };
 
+// visit https://mermaid.js.org/schemas/config.schema.json for default schema
 export const MERMAID_CONFIG = {
   startOnLoad: false,
   flowchart: { curve: "linear" },
